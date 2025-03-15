46abe4d3d9efa4f2b61067fa156e5d851171d877
adozoo <adozoo.c@gmail.com>
2025-03-15 21:10:33 +0900

    1

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
-from youtube_transcript_api import YouTubeTranscriptApi
+import os
+import time
 
-# 视频ID
-video_id = "iHrZKQQpuJQ"
 
-# 列出可用字母
-transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
-# print(transcript_list)
+def printENV():
+    # 获取所有环境变量
+    env_vars = os.environ
+    # 打印指定的环境变量
+    for key, value in env_vars.items():
+        if key in ["OPENAI_API_KEY", "TAVILY_API_KEY", "ANTHROPIC_API_KEY", "GOOGLE_API_KEY"]:
+            print(f"{key}: {value}")
 
-# 获取字幕
-transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
-# print(transcript)
-content = ""
-for item in transcript:
-    # print(item['text'])
-    if item['text'] != "[Music]":
-        content += item['text'] + " "
 
-print(content)
+def evalEndTime(start_time):
+    end_time = time.time()  # 获取结束时间
+    execution_time = "(程序运行时间：%.2f 秒)" % (
+        end_time - start_time
+    )  # 计算程序运行时间
+    return execution_time
 
-# 翻译字幕
-transcript = transcript_list.find_transcript(['en'])
-translated_transcript = transcript.translate('zh-Hans')
 
-content = ""
-for item in translated_transcript.fetch():
-    if item['text'] != "[音乐]":
-        content += item['text'] + ""
-print(content)
+# --- 以下为调用函数的代码 ---
+
+if __name__ == "__main__":
+    start = time.time()
+    
+    printENV()  # 调用函数，打印环境变量
+
+    # 程序主体可放在这里
+    time.sleep(1)  # 模拟程序运行1秒钟
+    
+    # 打印程序运行时间
+    print(evalEndTime(start))
